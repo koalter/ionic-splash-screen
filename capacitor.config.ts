f5ae93713241f1b01933c44bc88d88788be8c5c5
koalter <heavens_drive090@hotmail.com>
2023-09-23 19:30:23 -0300

    add @capacitor/splash-screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,22 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-splash-screen',
+  appId: 'io.ionic.splash',
+  appName: 'Ionic Splash Screen',
   webDir: 'www',
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      splashFullScreen: true,
+      splashImmersive: true
+    },
+  },
 };
 
 export default config;
